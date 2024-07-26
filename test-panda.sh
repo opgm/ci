97931d1251001981670703e74ae42f3a03bd1bba
Eric Brown <eric@ebrown.net>
2024-07-26 08:58:04 -0600

    Test panda in docker instead of on device

--- test-panda.sh
+++ test-panda.sh
@@ -1,13 +1,4 @@
 #!/bin/bash -e
 
-ci/deploy.sh
-ssh comma << 'ENDSSH'
-  set -e
-  cd /data/openpilot/panda
-  scons -j$(nproc)
-
-  cd /data/openpilot
-  python3 panda/board/flash.py
-  python3 -m pytest panda/tests/safety/test_gm.py
-
-ENDSSH
+docker build -t panda-test -f Dockerfile.openpilot .
+docker run --rm panda-test panda/tests/safety/test.sh
