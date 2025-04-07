4eae752f8acb400c9b91db0f9873775ac69f0ebf
Eric Brown <eric@ebrown.net>
2025-04-07 16:45:02 -0600

    Update precompilation script

--- precompile.sh
+++ precompile.sh
@@ -1,25 +1,13 @@
 #!/bin/bash -e
 # Copy all files over to comma to build
-branch=$(git rev-parse --abbrev-ref HEAD)
 scp ci/id_rsa_github comma:/data || exit 1
 ssh comma << EOF
+set -e
 rm -rf /data/openpilot /data/openpilot_build
-GIT_SSH_COMMAND="ssh -i /data/id_rsa_github -o StrictHostKeyChecking=no" git clone --single-branch --branch ${branch} --depth=1 git@github.com:opgm/openpilot.git /data/openpilot_build
+GIT_SSH_COMMAND="ssh -i /data/id_rsa_github -o StrictHostKeyChecking=no" git clone --branch master --recurse-submodules --filter=blob:none git@github.com:opgm/openpilot.git /data/openpilot_build
 cd /data/openpilot_build
-RELEASE_BRANCH=${branch} /usr/bin/bash -e -l release/build_release.sh
+RELEASE_BRANCH=${RELEASE_BRANCH} PANDA_DEBUG_BUILD=1 GIT_SSH_COMMAND="ssh -i /data/id_rsa_github" /usr/bin/bash -e -l release/build_release.sh
 EOF
 echo "Build successful"
 
 ssh comma -t "sudo reboot" || :
-
-## Copy all files back over
-#rm -rf build
-#(rsync -zarv comma:/data/openpilot/ build) 1> /dev/null
-#pushd build
-#[[ ! -f "body/board/obj/.placeholder" ]] && exit 1
-#[[ ! -f "panda/board/obj/.placeholder" ]] && exit 1
-#[[ ! -f "selfdrive/modeld/models/supercombo.thneed" ]] && exit 1
-## TODO: update readme
-#git push -f -u origin $branch --no-verify
-#popd
-#rm -rf build
