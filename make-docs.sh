9c85d74717af383d4c7473d556d030c322daffd6
Eric Brown <eric@ebrown.net>
2025-04-07 16:45:54 -0600

    Clean up CI

--- make-docs.sh
+++
@@ -1,2 +0,0 @@
-COMMIT=$(git rev-parse master) gomplate < opgm_readme.t.md > README.md
-git commit -am "Update docs"
--- test-panda.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-docker build -t panda-test -f Dockerfile.openpilot .
-docker run --rm panda-test panda/tests/safety/test.sh
