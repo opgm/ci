a7d8cd6d1f7283ba84cc13a0cdec8f6894bf9e6f
Eric Brown <eric@ebrown.net>
2025-04-07 19:42:21 -0600

    Clean up scripts

--- precompile.sh
+++ precompile.sh
@@ -1,13 +1,19 @@
 #!/bin/bash -e
-# Copy all files over to comma to build
+if [ -z "$RELEASE_BRANCH" ]; then
+  echo "RELEASE_BRANCH is not set"
+  exit 1
+fi
 scp ci/id_rsa_github comma:/data || exit 1
 ssh comma << EOF
 set -e
+export GIT_SSH_COMMAND="ssh -i /data/id_rsa_github -o StrictHostKeyChecking=no"
+export PANDA_DEBUG_BUILD=1
+export RELEASE_BRANCH=${RELEASE_BRANCH}
+
 rm -rf /data/openpilot /data/openpilot_build
-GIT_SSH_COMMAND="ssh -i /data/id_rsa_github -o StrictHostKeyChecking=no" git clone --branch master --recurse-submodules --filter=blob:none git@github.com:opgm/openpilot.git /data/openpilot_build
+git clone --branch master --recurse-submodules --filter=blob:none git@github.com:opgm/openpilot.git /data/openpilot_build || { echo "Failed to clone openpilot"; exit 1; }
 cd /data/openpilot_build
-/data/openpilot_build/hardware/tici/agnos.py /data/openpilot_build/hardware/tici/agnos.json --swap
-RELEASE_BRANCH=${RELEASE_BRANCH} PANDA_DEBUG_BUILD=1 GIT_SSH_COMMAND="ssh -i /data/id_rsa_github" /usr/bin/bash -e -l release/build_release.sh
+/usr/bin/bash -e -l /data/openpilot_build/release/build_release.sh
 EOF
 echo "Build successful"
 
--- update.sh
+++ update.sh
@@ -4,8 +4,6 @@ set -e
 pushd opendbc_repo
 git fetch origin master
 git checkout master
-git pull --ff-only origin master
-git submodule update
 git rebase origin/master
 git push --force-with-lease -u opgm master
 popd
@@ -14,8 +12,23 @@ git fetch origin master
 git checkout master
 git rebase --onto origin/master $(git merge-base HEAD origin/master) HEAD~1
 git checkout -B master
+
+# Check if system/hardware/tici/agnos.json was modified by the rebased commits
+if git diff --quiet origin/master HEAD -- system/hardware/tici/agnos.json; then
+  echo "system/hardware/tici/agnos.json not modified."
+  BUMP_AGNOS=0
+else
+  echo "system/hardware/tici/agnos.json was modified."
+  BUMP_AGNOS=1
+fi
+
 git add opendbc_repo
 git commit -m "Bump opendbc"
 git push --force-with-lease -u opgm master:master
+
+if [ "$BUMP_AGNOS" -eq 1 ]; then
+  echo "Unable to precompile pending AGNOS update; please run the script again after the AGNOS update is merged."
+  exit 1
+fi
 
 RELEASE_BRANCH=nightly ./ci/precompile.sh
